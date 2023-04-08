ce2659948e16de9522f687287d9bb85d2e44e637
UlyssesCortes <ulyssescortes13@gmail.com>
2023-04-08 00:59:08 -0700

    Hamburger icon added for mobile. Styling still needs to be done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navBar.css'
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const handleToggle = () => {
+        setShowLinks(!showLinks);
+    }
 
     return (
         <>
             <nav className='navBar'>
-                <h1 ><a href='#home' className='webTitle'>WINDOWPROS</a></h1>
-                <section className='linksSection'>
+                <h1><a href='#home' className='webTitle'>WINDOWPROS</a></h1>
+                <div className='toggleButton' onClick={handleToggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <section className={`linksSection ${showLinks ? 'show' : ''}`}>
                     <a href='#quote' className='links'>Get a quote</a>
                     <a href='#reviews' className='links'>Reviews</a>
                     <a href='#services' className='links'>Services</a>
